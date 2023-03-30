598212835323a3b302da355f04c0a6026620b1fb
femi <oluwafemi.akinyemi@datamellon.com>
2023-03-30 08:47:08 +0100

    Progress

--- src/Components/ThankyouPage/Thankyou.jsx
+++ src/Components/ThankyouPage/Thankyou.jsx
@@ -3,18 +3,18 @@ import thankyou from '../../Assets/images/icon-thank-you.svg'
 import './Thankyou.css'
 
 function Thankyou() {
-  return (
-      <div class="stepThankYou" id="stepThankYou">
-          <img src={thankyou} alt="" />
-          <h1>Thank you!</h1>
-          <p>
-              Thanks for confirming your subscription! We hope you have fun
-              using our platform. If you ever need support, please feel free to
-              email us at support@loremgaming.com.
-          </p>
-      </div>
+    return (
+        <div class="stepThankYou" id="stepThankYou">
+            <img src={thankyou} alt="" />
+            <h1>Thank you!</h1>
+            <p>
+                Thanks for confirming your subscription! We hope you have fun
+                using our platform. If you ever need support, please feel free to
+                email us at support@loremgaming.com.
+            </p>
+        </div>
 
-  )
+    )
 }
 
 export default Thankyou
